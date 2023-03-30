e06a6916eb0feccc7bb07d7600e972ec7df35685
Eugene Kim <eugene02876@gmail.com>
2023-03-31 00:39:43 +0900

    [feat]: tsconfig 세팅

--- apps/next.config.js
+++ apps/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+//모노레포에서 컴포넌트를 트랜스파일 해오는 과정이 필요하기 때문에 넣어둔 코드,(플러그인 깔음)
 const withTM = require('next-transpile-modules')([
   '@hc/ui',
   '@hc/utils',
