88dee750a2668589c20ef3433ad936bfd1ab4ba6
Eugene Kim <eugene02876@gmail.com>
2023-03-31 00:15:31 +0900

    [feat] : app 모노레포 세팅(스토리북)

--- apps/next.config.js
+++ apps/next.config.js
@@ -3,4 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+const withTM = require('next-transpile-modules')([
+  '@hc/ui',
+  '@hc/utils',
+]);
+module.exports = withTM({
+  // Any additional config for next goes in here
+});
+
 module.exports = nextConfig
