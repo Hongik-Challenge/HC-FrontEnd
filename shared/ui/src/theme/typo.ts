4bdf6b485f729d8670b527c3891a3f6aa3310a4c
Eugene Kim <eugene02876@gmail.com>
2023-06-01 11:40:17 +0900

    [feature]: 디자인 시스템 변경사항 추가

--- shared/ui/src/theme/typo.ts
+++ shared/ui/src/theme/typo.ts
@@ -2,72 +2,103 @@ import { css } from "styled-components";
 
 export const calcRem = (px: number) => `${px / 16}rem`;
 
-//Typo 지정..............
 export const typo = {
-  Header_24: css`
+  Headline2: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(24)};
     line-height: 140%;
     font-weight: 700;
   `,
-  Header_20: css`
+  Headline1: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(20)};
     line-height: 140%;
     font-weight: 600;
   `,
-  SubTitle_18: css`
+  Subtitle3: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(18)};
     line-height: 140%;
     font-weight: 600;
   `,
-  SubTitle_16: css`
+  Subtitle2: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(16)};
     line-height: 140%;
     font-weight: 600;
   `,
-  SubTitle_14: css`
+  Subtitle1: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(14)};
     line-height: 140%;
     font-weight: 600;
   `,
-  Text_16_M: css`
-    font-family: 'Pretendard';
-    font-size: ${calcRem(16)};
-    line-height: 160%;
-    font-weight: 500;
-  `,
-  Text_16_R: css`
-    font-family: 'Pretendard';
-    font-size: ${calcRem(16)};
-    line-height: 160%;
-    font-weight: 400;
-  `,
-  Text_14_M: css`
+  Subtitle1_1: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(14)};
     line-height: 160%;
-    font-weight: 500;
-  `,
-  Text_14_R: css`
-    font-family: 'Pretendard';
-    font-size: ${calcRem(14)};
-    line-height: 160%;
-    font-weight: 400;
+    font-weight: 600;
+    text-decoration: underline;
 `,
-    Caption_12_M: css`
+  Body2_2: css`
+    font-family: 'Pretendard';
+    font-size: ${calcRem(16)};
+    line-height: 160%;
+    font-weight: 500;
+  `,
+  Body2_1: css`
+    font-family: 'Pretendard';
+    font-size: ${calcRem(16)};
+    line-height: 160%;
+    font-weight: 400;
+  `,
+  Body1_2: css`
+    font-family: 'Pretendard';
+    font-size: ${calcRem(14)};
+    line-height: 160%;
+    font-weight: 500;
+  `,
+  Body1_1: css`
+    font-family: 'Pretendard';
+    font-size: ${calcRem(14)};
+    line-height: 160%;
+    font-weight: 400;
+  `,
+  Caption3_3: css`
+   font-family: 'Pretendard';
+   font-size: ${calcRem(12)};
+   line-height: 160%;
+   font-weight: 600;   
+ `,
+  Caption3_2: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(12)};
     line-height: 160%;
     font-weight: 500;   
-    `,
-    Caption_12_R: css`
+  `,
+  Caption3_2_1: css`
+    font-family: 'Pretendard';
+    font-size: ${calcRem(12)};
+    line-height: 160%;
+    font-weight: 500;  
+    text-decoration: underline; 
+  `,
+  Caption3_1: css`
     font-family: 'Pretendard';
     font-size: ${calcRem(12)};
     line-height: 160%;
     font-weight: 400;   
-    `,
+  `,
+  Caption2_1: css`
+    font-family: 'Pretendard';
+    font-size: ${calcRem(10)};
+    line-height: 160%;
+    font-weight: 400;   
+  `,
+  Caption1: css`
+    font-family: 'Pretendard';
+    font-size: ${calcRem(8)};
+    line-height: 160%;
+    font-weight: 400;   
+  `,
 } as const;
