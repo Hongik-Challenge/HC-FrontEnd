7b3f6a214d9ca170251f7031a936820336a41b57
Eugene Kim <eugene02876@gmail.com>
2023-06-06 17:14:25 +0900

    [feature]: Tag, Button, Badge 구현

--- shared/ui/src/components/Tag/index.tsx
+++ shared/ui/src/components/Tag/index.tsx
@@ -1,13 +1,78 @@
 import styled from "styled-components";
-import { theme } from "../../theme";
+import { KeyOfColor, KeyOfTypo } from "../../theme";
+import { css } from "styled-components";
 
-export const Tag = () => {
-  return(
-    <div>
-      하이룽
-    </div>
-  )
+type TagVariant = 'filter' | 'keyword';
+type TagColorVariant  = 'basic' | 'select' | 'key'
+type TagSize = {
+  [key in TagVariant]:{
+    padding: string;
+    height: string;
+    typo: KeyOfTypo;
+    radius: number;
+  }
 }
-const Wrapper = styled.div`
-  background-color: ${theme.palette.black};
-`
+type TagColor = {
+  [key in TagColorVariant]: {
+    backgroundColor: KeyOfColor;
+    fontColor: KeyOfColor;
+    radiusColor?: KeyOfColor;
+  }
+}
+const TAG_SIZE : TagSize = {
+  filter: {
+    padding: '8px 12px',
+    height: '35px',
+    typo: 'Caption3_2',
+    radius: 32
+  },
+  keyword: {
+    padding: '1px 9px',
+    height: '18px',
+    typo: 'Caption2_1',
+    radius: 17
+  }
+}
+
+const TAG_COLOR : TagColor = {
+  basic: {
+    backgroundColor: 'black_1100',
+    fontColor: 'black',
+    radiusColor: 'black_900'
+  },
+  select: {
+    backgroundColor: 'blue_500',
+    fontColor: 'white',
+  },
+  key: {
+    backgroundColor: 'black_1000',
+    fontColor: 'black', 
+  }
+}
+
+export interface TagProps extends React.ComponentProps<'div'> {
+  color: TagColorVariant;
+  variant: TagVariant;
+}
+
+export const Tag = ({children, color, variant}:TagProps) => {
+  return(
+    <Wrapper color = {color} variant ={variant}>
+      {children}
+    </Wrapper>
+  )
+};
+
+const Wrapper = styled.div<{color: TagColorVariant, variant: TagVariant}>`
+  ${({ variant, theme }) => theme.typo[TAG_SIZE[variant].typo]};
+  border-radius: ${({variant}) => (`${TAG_SIZE[variant].radius}px`)};
+  background-color: ${({color, theme}) => theme.palette[TAG_COLOR[color].backgroundColor]};
+  padding: ${({variant}) => TAG_SIZE[variant].padding};
+  height: ${({variant}) => TAG_SIZE[variant].height};
+  ${({color}) => color === 'basic' &&
+  css`
+    border : 1px solid #ECECEC;
+  `}
+  display: inline-block;
+  color : ${({color, theme}) => theme.palette[TAG_COLOR[color].fontColor]};
+`;
